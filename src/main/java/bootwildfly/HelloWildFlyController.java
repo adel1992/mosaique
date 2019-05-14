54615989102fd54f7d856200f0af611937ad1db4
adel1992 <adelmej1992@gmail.com>
2019-05-14 19:06:40 +0100

    streaming

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -1,7 +1,14 @@
 package bootwildfly;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class HelloWildFlyController {
@@ -11,4 +18,10 @@ public class HelloWildFlyController {
     public String sayHello(){
         return ("Hello, SpringBoot on Wildfly");
     }
+    
+    @RequestMapping("getStreaming")
+	void getStreaming(HttpServletResponse response) throws MalformedURLException, IOException{
+		response.setContentType("audio/mpeg");
+		IOUtils.copy(RadioConnector.getRadioStream("http://radio.mosaiquefm.net:8000/mosalive"), response.getOutputStream());
+	}
 }
